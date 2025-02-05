c9bf354ba72244c0ea2f944d4700bd7969443372
Erremme <Erremme2.0@gmail.com>
2025-02-05 18:20:14 +0100

    feat: Axios request and rendered the data on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 export default function App(){
-  
+   const [blogList , setBlogList ] = useState([])
+   const fetchProducts = () =>{
+    axios.get("http://localhost:3000/prodotti").then(function (res){
+      setBlogList(res.data)
+      
+    })
+   }
+    useEffect( fetchProducts , []);
+    console.log(blogList)
+
+    
+
+    
+
+ return(
+       <div className="container">
+        <h1>Food blog</h1>
+        <div className="row">
+         {blogList.map((item) => {
+          return(
+            <div class="card" key={item.id}>
+              <button class="delete-btn">×</button>
+            <img src={item.immagine}alt={item.titolo}/>
+            <div class="card-content">
+                <div class="card-title">{item.titolo}</div>
+                <div class="card-text">{item.descrizione}.</div>
+            </div>
+        </div>
+                
+          )
+         })}
+
+          
+      </div> 
+       </div>
+ )
 }
